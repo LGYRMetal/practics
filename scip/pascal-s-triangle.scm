a6614f92d3b00c4ad21144da39146cb387454ced
LGYRMetal <LGYRMetal@gmail.com>
2016-08-17 20:22:21 +0800

    implement pascal's triangle, but need to improve

--- scip/pascal-s-triangle.scm
+++ scip/pascal-s-triangle.scm
@@ -1,3 +1,28 @@
 ; Pascal's Triangle
 (define (pascal-triangle n)
-  (cond ((= n 1) (make-vector ))))
+  (if (< n 1)
+    (format
+      #t
+      "~%Procedure pascal-triangle~%requires its argument ~
+      to be of type integer greater then 0.")
+      (do ((i 1 (+ i 1)))
+        ((> i n))
+        (display-n-white-space (- n i))
+        (do ((j 1 (+ j 1)))
+          ((> j i) (newline))
+          (display (pt i j))
+          (cond ((< (pt i j) 10) (display-n-white-space 3))
+                ((< (pt i j) 100) (display-n-white-space 2))
+                ((< (pt i j) 1000) (display-n-white-space 1))
+                (else (display-n-white-space 1)))))))
+
+(define (pt n count)
+  (if (or (= n 1) (= n count) (= count 1))
+    1
+    (+ (pt (- n 1) (- count 1))
+       (pt (- n 1) count))))
+
+(define (display-n-white-space n)
+  (do ((i 1 (+ i 1)))
+    ((> i n))
+    (display " ")))
