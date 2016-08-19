ac1e80c924fd810a99d4f691a0aa0cefb2c5d4a7
LGYRMetal <LGYRMetal@gmail.com>
2016-08-19 17:27:53 +0800

    update wordpress (image.php)

--- wordpress/lgyshape/inc/template-tags.php
+++ wordpress/lgyshape/inc/template-tags.php
@@ -104,5 +104,56 @@ function shape_content_nav( $nav_id ) {
                                            is_archive() ||
                                            is_search() ) )
         return;
+
+    $nav_class = 'site-navigation paging-navigation';
+    if ( is_single() )
+        $nav_class = 'site-navigation post-navigation';
+
+    ?>
+    <nav role="navigation"
+         id="<?php echo $nav_id; ?>"
+         class="<?php echo $nav_class; ?>">
+        <h1 class="assistive-text">
+            <?php _e( 'Post navigation', 'shape' ); ?>
+        </h1>
+
+    <?php if ( is_single() ) : // navigation links for single post ?>
+        <?php previous_post_link(
+            '<div class="nav-previous">%link</div>',
+            '<span class="meta-nav">' .
+            _x( '&larr;', 'Previous post link', 'shape' ) .
+            '</span> %title'
+        ); ?>
+        <?php next_post_link(
+            '<div class="nav-next">%link</div>',
+            '%title <span class="meta-nav">' .
+            _x( '&rarr;', 'Next post link', 'shape' ) .
+            '</span>'
+        ); ?>
+
+    <?php //navigation links for home, archive, and search pages
+    elseif ( $wp_query->max_num_pages > 1 &&
+        ( is_home() || is_archive() || is_search() ) ) : ?>
+
+        <?php if ( get_next_posts_link() ) : ?>
+            <div class="nav-previous">
+                <?php next_posts_link( __(
+                    '<span class="meta-nav">&larr;</span> Older posts',
+                    'shape'
+                ) ); ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if ( get_previous_posts_link() ) : ?>
+            <div class="nav-next">
+                <?php previous_posts_link( __(
+                    'Newer posts <span class="meta-nav">&rarr;</span>',
+                    'shape'
+                ) ); ?>
+            </div>
+        <?php endif; ?>
+    <?php endif; ?>
+    </nav><!-- #<?php echo $nav_id; ?> -->
+<?php
 }
 endif; // shape_content_nav
--- wordpress/lgyshape/index.php
+++ wordpress/lgyshape/index.php
@@ -17,6 +17,9 @@ get_header(); ?>
 <div id="primary" class="content-area">
     <div id="content" class="site-content">
     <?php if ( have_posts() ) : ?>
+
+        <?php shape_content_nav( 'nav-above' ); ?>
+
         <?php /* Start the Loop */ ?>
         <?php while ( have_posts() ) : the_post(); ?>
             <?php
@@ -27,6 +30,11 @@ get_header(); ?>
             get_template_part( 'content', get_post_format() );
             ?>
         <?php endwhile; ?>
+
+        <?php shape_content_nav( 'nav-below' ); ?>
+
+    <?php else : ?>
+        <?php get_template_part( 'no-results', 'index' ); ?>
     <?php endif; ?>
     </div><!-- #content .site-content -->
 </div><!-- #primary .content-area -->
