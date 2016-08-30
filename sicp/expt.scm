a4f6faab7d223fc5239373757e831c4592f00b4a
LGYRMetal <LGYRMetal@gmail.com>
2016-08-30 20:44:17 +0800

    implement the iteration version of exponent; practics 1.16

--- sicp/expt.scm
+++ sicp/expt.scm
@@ -2,9 +2,10 @@
 (define (my-expt b n)
   (my-expt-iter b n 1))
 
-(define (my-expt-iter b n product)
-  (cond ((= n 1) product)
-        ((even? n) (my-expt-iter product (/ n 2) (square b)))))
+(define (my-expt-iter b n a)
+  (cond ((= n 0) a)
+        ((even? n) (my-expt-iter (square b) (/ n 2) (square (* a b))))
+        (else (* b (my-expt-iter b (- n 1) 1)))))
 
 (define(square n)
   (* n n))
