59c4868eca1aafee887496cfa64cc696e4de3aaf
LGYRMetal <LGYRMetal@gmail.com>
2016-08-30 22:58:19 +0800

    expt.scm add a new version

--- sicp/expt.scm
+++ sicp/expt.scm
@@ -9,3 +9,12 @@
 
 (define(square n)
   (* n n))
+
+;;;;;; another version ;;;;;;
+(define (my-expt-iter-1 b n a)
+  (cond ((= n 0) a)
+        ((even? n) (my-expt-iter-1 (square b) (/ n 2) (square (* a b))))
+        (else (* b (my-expt-1 b (- n 1))))))
+
+(define (my-expt-1 b n)
+  (my-expt-iter-1 b n 1))
