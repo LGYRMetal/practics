a8c3373445d41e027dc8a4a132f8b76156ef1076
LGYRMetal <LGYRMetal@gmail.com>
2016-08-31 13:00:03 +0800

    upgrade the expt.scm

--- sicp/expt.scm
+++ sicp/expt.scm
@@ -1,20 +1,36 @@
 ; practics 1.16
+
+;;;;;; 综合下面两个算法，得出下面最佳算法 ;;;;;
 (define (my-expt b n)
   (my-expt-iter b n 1))
 
 (define (my-expt-iter b n a)
   (cond ((= n 0) a)
-        ((even? n) (my-expt-iter (square b) (/ n 2) (square (* a b))))
-        (else (* b (my-expt-iter b (- n 1) 1)))))
+        ((even? n) (my-expt (square b) (/ n 2)))
+        (else (* b (my-expt b (- n 1))))))
+
+; version 1
+(define (my-expt-1 b n)
+  (my-expt-iter-1 b n 1))
+
+(define (my-expt-iter-1 b n a)
+  (cond ((= n 0) a)
+        ;((even? n) (my-expt-iter (square b) (/ n 2) (square (* a b))))
+        ; (square (* a b))这部分纯属多余，没有用，因为在这步a的值是多少都
+        ; 不会影响结果
+        ((even? n) (my-expt-iter-1 (square b) (/ n 2) 1))
+        (else (* b (my-expt-iter-1 b (- n 1) 1)))))
 
 (define(square n)
   (* n n))
 
 ;;;;;; another version ;;;;;;
-(define (my-expt-iter-1 b n a)
+; version 2
+(define (my-expt-iter-2 b n a)
   (cond ((= n 0) a)
-        ((even? n) (my-expt-iter-1 (square b) (/ n 2) (square (* a b))))
-        (else (* b (my-expt-1 b (- n 1))))))
+        ((even? n) (my-expt-iter-2 (square b) (/ n 2) (square (* a b))))
+        (else (* b (my-expt-2 b (- n 1))))))
 
-(define (my-expt-1 b n)
-  (my-expt-iter-1 b n 1))
+(define (my-expt-2 b n)
+  (my-expt-iter-2 b n 1))
+
