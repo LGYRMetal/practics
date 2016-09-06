3f021da44d630180e12b727262d32fafde9597ca
LGYRMetal <LGYRMetal@gmail.com>
2016-09-06 20:22:40 +0800

    practics 1.18

--- sicp/multiplication.scm
+++ sicp/multiplication.scm
@@ -1,17 +1,17 @@
-; practics 1.17
-
 ; example
 (define (mul a b)
   (if (= b 0)
     0
     (+ a (mul a (- b 1)))))
 
+; practics 1.17
 ; recursion version
 (define (fast-mul a b)
   (cond ((= b 0) 0)
         ((even? b) (double (fast-mul a (halve b))))
         (else (+ a (fast-mul a (- b 1))))))
 
+; practics 1.18
 ; iteration version
 (define (fast-mul-i a b)
   (fast-mul-iter a b 0))
