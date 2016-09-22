50e5e3714eec637bc0dc8445d6c82d9323f1d314
LGYRMetal <LGYRMetal@gmail.com>
2016-09-22 17:30:32 +0800

    update drop-sql-table

--- drop_wp_tables.sql
+++ drop_wp_tables.sql
@@ -10,3 +10,5 @@ drop table wordpress.wp_termmeta;
 drop table wordpress.wp_terms;
 drop table wordpress.wp_usermeta;
 drop table wordpress.wp_users;
+
+mysql -u root -p198708060316 -N -s information_schema -e "select concat('drop table if exists ', table_name, ';') from tables where table_schema='djangodb'" | mysql -u root -p198708060316 -f djangodb
