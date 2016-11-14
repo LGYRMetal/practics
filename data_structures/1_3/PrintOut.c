d6a7851f840d67dfe69913ecfd2cd78fbe296216
LGYRMetal <LGYRMetal@gmail.com>
2016-11-14 17:30:59 +0800

    data structures 1.3

--- data_structures/1_3/PrintOut.c
+++ data_structures/1_3/PrintOut.c
@@ -3,6 +3,17 @@
 
 #define PrintDigit(ch)  (putchar((ch) + '0'))
 
+int power(int x, int y)
+{
+    int result = 1;
+    for(int i = 0; i < y; i++)
+    {
+        result *= x;
+    }
+
+    return result;
+}
+
 void PrintInt(unsigned int n)
 {
     if(n >= 10)
@@ -12,6 +23,8 @@ void PrintInt(unsigned int n)
 
 void PrintOut(double n)
 {
+    int accuracy = 3; /* 精度，希望输出极为小数就改成几 */
+    printf("power.....%d\n", power(10, 9));
     printf("%f\n", n);
     if(n < 0)
     {
@@ -26,8 +39,10 @@ void PrintOut(double n)
         putchar('.');
 
         n -= i;
-        i = n * 1000;
-        int r = (int)(n * 10000) - i * 10; /* 用来判断是否四舍五入 */
+        i = n * power(10, accuracy);
+        printf("here.......%d\n", i);
+        /* r用来判断是否四舍五入 */
+        int r = (int)(n * power(10, accuracy + 1)) - i * 10;
         if(r >= 5)
             i++;
 
