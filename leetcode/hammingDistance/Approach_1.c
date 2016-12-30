c0d341e2ef0a4c91db48d914937b76a01e0ecaf8
LGYRMetal <LGYRMetal@gmail.com>
2016-12-30 10:12:46 +0800

    hamming distance

---
+++ leetcode/hammingDistance/Approach_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <err.h>
+
+int hammingDistance(int x, int y)
+{
+    int i;
+    int hd = 0;
+    int z = x ^ y;
+
+    for(i = 0; i < 8 * sizeof(int) && z != 0; i++) {
+        hd += (z & 0x0001 == 1) ? 1 : 0;
+        z >>= 1;
+    }
+
+    return hd;
+}
+
+int main()
+{
+    printf("%d\n", hammingDistance(1, 4));
+    return 0;
+}
