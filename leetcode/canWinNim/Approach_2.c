56eef6e028e944a534e2cbd07fbd921c67f8ff0e
LGYRMetal <LGYRMetal@gmail.com>
2017-02-05 17:20:25 +0800

    canWinNim ok

--- leetcode/canWinNim/Approach_2.c
+++ leetcode/canWinNim/Approach_2.c
@@ -3,19 +3,12 @@
 
 typedef unsigned short bool;
 
-const char true = 1;
-const char false = 0;
-
 bool canWinNim(int n)
 {
-    if(n <= 3)
-        return true;
-    else if(canWinNim(n-1) && canWinNim(n-2) && canWinNim(n-3))
-        /* no matter 1, 2, or 3 strones you remove, you always can't win */
-        return false;
-    return true;
+    return n % 4;
 }
 
+/*
 int main(int argc, char* argv[])
 {
     int n = atoi(argv[1]);
@@ -27,15 +20,14 @@ int main(int argc, char* argv[])
         printf("no\n");
     return 0;
 }
-/*
+*/
 int main(int argc, char* argv[])
 {
     for(int i = 1; i < 100; i++) {
         if(!canWinNim(i)) {
-            printf("%d\n", i/4);
+            printf("%d\n", i);
         }
     }
 
     return 0;
 }
-*/
