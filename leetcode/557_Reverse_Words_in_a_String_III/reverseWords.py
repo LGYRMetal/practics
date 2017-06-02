a3c935014ae9849de3818397e41b26a2935440d8
LGYRMetal <LGYRMetal@gmail.com>
2017-06-02 14:48:09 +0800

    leetcode 557 reverse words in a string iii

--- leetcode/557_Reverse_Words_in_a_String_III/reverseWords.py
+++ leetcode/557_Reverse_Words_in_a_String_III/reverseWords.py
@@ -5,4 +5,6 @@ class Solution(object):
         :rtype: str
         """
        #return ' '.join(list(map(''.join, list(map(list, map(reversed, s.split()))))))
-        return ' '.join(map(''.join,  map(reversed, s.split())))
+        return ' '.join(map(''.join,  map(reversed, s.split()))) # my
+       #return ' '.join(x[::-1] for x in s.split())
+       #return ' '.join(s.split()[::-1])[::-1]
