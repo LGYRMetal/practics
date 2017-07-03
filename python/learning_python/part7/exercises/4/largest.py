76d989b4dbaa7ebdfebca10a9161190e9a34624e
LGYRMetal <LGYRMetal@gmail.com>
2017-07-03 17:30:44 +0800

    largest.py

--- python/learning_python/part7/exercises/4/largest.py
+++ python/learning_python/part7/exercises/4/largest.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 
+# Find the largest Python source file in a single directory
+
 import os, glob
 dirname = r'/usr/lib/python3.5'
 
@@ -7,4 +9,8 @@ allsizes = []
 allpy = glob.glob(dirname + os.sep + '*.py')
 for filename in allpy:
     filesize = os.path.getsize(filename)
+    allsizes.append((filesize, filename))
 
+allsizes.sort()
+print(allsizes[:2])
+print(allsizes[-2:])
