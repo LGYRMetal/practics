dc25fde3a7ba8c3dec6ad7a702423c511f50b557
LGYRMetal <LGYRMetal@gmail.com>
2017-10-12 16:29:11 +0800

    java

--- java/test/CalendarTest.java
+++ java/test/CalendarTest.java
@@ -2,12 +2,24 @@
  * 获取2008年8月8日是星期几
  */
 import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class CalendarTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Calendar c = Calendar.getInstance(); // 获取系统当前日历
+
+        /* 这是我的思路 下面是视频的思路 c.setTime
         c.set(2008, Calendar.AUGUST, 8); // 设置日历时间为2008年8月8日
         //c.setFirstDayOfWeek(Calendar.MONDAY);
+        */
+
+        // 获取2008年8月8日的日历
+        String strTime = "2008,08,08";
+        Date d = new SimpleDateFormat("yyyy,MM,dd").parse(strTime);
+
+        // 该日历就是2008年8月8日的日历
+        c.setTime(d);
     
         // 获取2008年8月8日是一星期中的第几天
         int i = c.get(Calendar.DAY_OF_WEEK);
