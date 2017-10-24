70ba6dc084887aab716d50a3861fad57ab518483
LGYRMetal <LGYRMetal@gmail.com>
2017-10-24 17:33:10 +0800

    java

--- java/test/ArrayCopy.java
+++ java/test/ArrayCopy.java
@@ -3,7 +3,7 @@
  */
 import java.util.Arrays;
 public class ArrayCopy {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int[] src = {1, 2, 3, 4};
         int[] dest = new int[4];
 
@@ -20,10 +20,73 @@ public class ArrayCopy {
     /*
      * 拷贝数组的数据到另一个数组
      */
+    /*
     public static void copy(int[] src,  int srcPos,
                             int[] dest, int destPos, int length) {
         for(int i = 0; i < length; i++) {
             dest[destPos++] = src[srcPos++];
         }
+    }
+    */
+
+    public static void copy(Object src, int srcPos,
+                            Object dest, int destPos, int length)
+        throws Exception {
+        if(src == null || dest == null) {
+            throw new NullPointerException("传入的数组是null");
+        }
+
+        switch(src.getClass().getName()) {
+            case "[B":
+                for(int i = 0; i < length; i++) {
+                    ((byte[])dest)[destPos++] = ((byte[])src)[srcPos++];
+                }
+                break;
+            case "[S":
+                for(int i = 0; i < length; i++) {
+                    ((short[])dest)[destPos++] = ((short[])src)[srcPos++];
+                }
+                break;
+            case "[I":
+                for(int i = 0; i < length; i++) {
+                    ((int[])dest)[destPos++] = ((int[])src)[srcPos++];
+                }
+                break;
+            case "[J":
+                for(int i = 0; i < length; i++) {
+                    ((long[])dest)[destPos++] = ((long[])src)[srcPos++];
+                }
+                break;
+            case "[F":
+                for(int i = 0; i < length; i++) {
+                    ((float[])dest)[destPos++] = ((float[])src)[srcPos++];
+                }
+                break;
+            case "[D":
+                for(int i = 0; i < length; i++) {
+                    ((double[])dest)[destPos++] = ((double[])src)[srcPos++];
+                }
+                break;
+            case "[Z":
+                for(int i = 0; i < length; i++) {
+                    ((boolean[])dest)[destPos++] = ((boolean[])src)[srcPos++];
+                }
+                break;
+            case "[C":
+                for(int i = 0; i < length; i++) {
+                    ((char[])dest)[destPos++] = ((char[])src)[srcPos++];
+                }
+                break;
+            default:
+                Class c = Class.forName(src.getClass().getName());
+                c.cast(src)
+        }
+    }
+}
+
+class NullPointerException extends Exception {
+    public NullPointerException() {}
+    public NullPointerException(String msg) {
+        super(msg);
     }
 }
