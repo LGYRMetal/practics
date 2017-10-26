25c251673c4246679694c8fcb96f32a347ad1521
LGYRMetal <LGYRMetal@gmail.com>
2017-10-26 10:27:34 +0800

    java

---
+++ java/test/NullTest.java
@@ -0,0 +1,6 @@
+public class NullTest {
+    public static void main(String[] args) {
+        //NullTest.java:3: 错误: 无法取消引用<空值>
+        //System.out.println(null.startWith("a"));
+    }
+}
---
+++ java/test/StartWithTest.java
@@ -0,0 +1,5 @@
+public class StartWithTest {
+    public static void main(String[] args) {
+        System.out.println((new String("public")).startsWith("public"));
+    }
+}
