906979e4bfafabb7be75e5338c7b206b29da7c30
LGYRMetal <LGYRMetal@gmail.com>
2017-11-27 09:43:26 +0800

    netcopyfile

---
+++ java/net/tcp/netcopyfile/NetCopyFileServer.java
@@ -0,0 +1,29 @@
+package com.lgyremtel;
+
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class NetCopyFileServer implements Runnable {
+    // Field
+    public static final int LISTENING_PORT = 65535;
+    private Socket socket;
+
+    // Constructor
+    public NetCopyFileServer(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+    }
+
+    public static void main(String[] args) {
+        // 1. Create Server Socket, and bind listening port.
+        ServerSocket ss = new ServerSocket(LISTENING_PORT);
+
+        // 2. Wait the coming Socket throught the ServerSocket's accept
+        //    method.
+        Socket s = ss.accept();
+
+        // 3. 
+    }
+}
