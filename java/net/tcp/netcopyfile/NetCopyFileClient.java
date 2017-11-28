fe9ab26206cbb1f92042352ae1e4cc97943766cb
LGYRMetal <LGYRMetal@gmail.com>
2017-11-28 17:38:22 +0800

    netcopyfile

---
+++ java/net/tcp/netcopyfile/NetCopyFileClient.java
@@ -0,0 +1,67 @@
+package com.lgyrmetal;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.net.Socket;
+
+public class NetCopyFileClient {
+    public static void main(String[] args) {
+        // Get the file name, destination ip, destination port
+        if(args.length != 2) {
+            usage("缺少必要的参数!");
+            return;
+        }
+
+        String fileName = args[0];
+        if(fileName.isEmpty()) {
+            usage("无效的文件名!");
+            return;
+        }
+
+        String destination = args[1];
+        if(!destination.matches("(((25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d))))\\:((6553[0-5])|(655[0-2]\\d)|(65[0-4]\\d{2})|(6[0-4]\\d{3})|(([1-5]\\d{4})|([1-9]\\d{3})|([1-9]\\d{2})|([1-9]?\\d)))")) {
+            usage("无效的目标IP地址");
+            return;
+        }
+
+        String[] splitDestination = destination.split(":");
+        String ip = splitDestination[0];
+        int port = Integer.valueOf(splitDestination[1]).intValue();
+
+        // 1. create Client Socket
+        try(Socket s = new Socket(ip, port)) {
+            // 2. Get the io Stream of Socket
+            InputStream in = s.getInputStream();
+            OutputStream out = s.getOutputStream();
+
+            // 3. Send the file name first.
+            out.write((fileName + "\n").getBytes());
+
+            // 4. Prepare file content, and then send data.
+            FileInputStream fis = new FileInputStream(fileName);
+            int len = 0;
+            byte[] buf = new byte[1024];
+            while((len = fis.read(buf, 0, buf.length)) != -1) {
+                out.write(buf, 0, len);
+            }
+
+            // 5. Accept feedback if copied failed.
+            while((len = in.read(buf, 0, buf.length)) != -1) {
+                System.out.write(buf, 0, len);
+            }
+        } catch(Exception e) {
+            System.out.println("拷贝文件失败!");
+            e.printStackTrace();
+        }
+    }
+
+    private static void usage(String errMsg) {
+        System.out.println(errMsg);
+        System.out.println("Usage:");
+        System.out.println("java " +
+                Class.forName("NetCopyFileClient").getName() +
+                " filename" + " destinationIP:port");
+    }
+}
--- java/net/tcp/netcopyfile/NetCopyFileServer.java
+++ java/net/tcp/netcopyfile/NetCopyFileServer.java
@@ -1,8 +1,9 @@
 package com.lgyremtel;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import java.net.Socket;
@@ -22,16 +23,51 @@ public class NetCopyFileServer implements Runnable {
         /*
          * Deal with every connected Socket
          */
-        // 1. Get the socket's io stream.
+        try {
+            // 1. Get the socket's InputStream.
+            InputStream in = socket.getInputStream();
+
+            // 2. Get the file name which is being copied.
+            String fileName =
+                new BufferedReader(new InputStreamReader(in)).readLine();
+
+            // 3. Get the file content start point.
+            // Get the bytes length of the file name.
+            int fileNameLen = fileName.getBytes().length;
+            // The start of the file content is fileNameLen +
+            // "\n".getBytes().length
+            int fileContentStart = fileNameLen + "\n".getBytes().length;
+
+            // 4. Read the file content, and output the content to local.
+            int len = in.available();
+            byte[] buf = new byte[len];
+            in.read(buf, fileContentStart, len);
+
+            FileOutputStream fos = new FileOutputStream(fileName, true);
+            fos.write(buf);
+            fos.flush();
+
+        } catch(Exception e) {
+            // 5. return feedback info
+            try(OutputStream out = socket.getOutputStream()) {
+                out.write("拷贝文件到服务器失败!".getBytes());
+            } catch(Exception eInner) {
+                eInner.printStackTrace();
+            }
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
+        Socket s = null;
         // 1. Create Server Socket, and bind listening port.
-        ServerSocket ss = new ServerSocket(LISTENING_PORT);
-
-        // 2. Wait the coming Socket throught the ServerSocket's accept
-        //    method.
-        Socket s = ss.accept();
+        try(ServerSocket ss = new ServerSocket(LISTENING_PORT)) {
+            // 2. Wait the coming Socket throught the ServerSocket's accept
+            //    method.
+            s = ss.accept();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
         // 3. Throw the accepted socket to the new thread to deal with.
         new Thread(new NetCopyFileServer(s)).start();
