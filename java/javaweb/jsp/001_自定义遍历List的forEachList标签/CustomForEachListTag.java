f6b248dfba31282399f59d5db637919c7ab2b1f0
LGYRMetal <LGYRMetal@gmail.com>
2018-02-12 17:27:20 +0800

    CustomForEachList

--- java/javaweb/jsp/001_自定义遍历List的forEachList标签/CustomForEachListTag.java
+++ java/javaweb/jsp/001_自定义遍历List的forEachList标签/CustomForEachListTag.java
@@ -1,11 +1,16 @@
 package com.lgy.jsp.tags;
 
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+
 import java.utils.List;
 import java.utils.ArrayList;
 
+
 public class CustomForEachListTag extends SimpleTagSupport {
     private List<String> items;
-    private String var;
+    private String var; // 当前遍历对象要放入域中的属性名
 
     public void setItems(List<String> items) {
         this.items = items;
@@ -15,8 +20,13 @@ public class CustomForEachListTag extends SimpleTagSupport {
         this.var = var;
     }
 
-    void doTag() {
-        for(this.var : this.items) {
+    void doTag() throws JspException, IOException {
+        for(Object obj : this.items) {
+            // 由于EL只能处理四大域中的数据，所以这里将当前遍历对象放入到
+            // page域中，其key为var所指定的名称，而value则为当前的遍历对象
+            // obj
+            this.getJspContext().setAttribute(var, obj);
+            this.getJspBody().invoke(null);
         }
     }
 }
