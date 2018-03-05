51ffae765ffa7f7c705263e9d64dd1019064e04b
LGYRMetal <LGYRMetal@gmail.com>
2018-03-05 17:28:09 +0800

    oracle sql

---
+++ oracle/note/SQL练习.sql
@@ -0,0 +1,140 @@
+﻿select *
+from emp;
+
+select *
+from users;
+
+select *
+from emp
+for update;
+
+alter session set language="en_US.utf-8";
+
+alter session set nls_language="AMERICAN";
+
+INSERT INTO EMP VALUES
+(7788,'SCOTT','ANALYST',7566,to_date('13-JUL-87')-85,3000,NULL,20);
+
+INSERT INTO EMP VALUES
+(7876,'ADAMS','CLERK',7788,to_date('13-JUL-87')-51,1100,NULL,20);
+
+INSERT INTO EMP VALUES
+(7877,'LGY','CLERK',7788,to_date('13-3月-2018'),2000,NULL,20);
+
+
+
+
+
+-- 1. 选择部门30中的雇员
+SELECT *
+FROM EMP
+WHERE DEPTNO = 30;
+
+-- 2. 列出所有办事员的姓名、编号和部门
+SELECT EMPNO 员工编号, ENAME 姓名, JOB 岗位, DEPTNO 部门编号
+FROM EMP
+WHERE JOB = 'CLERK';
+
+-- 3. 找出佣金高于薪金的雇员
+SELECT *
+FROM EMP
+WHERE COMM > SAL;
+
+-- 4. 找出佣金高于薪金60%的雇员
+SELECT *
+FROM EMP
+WHERE SAL * 0.6 = COMM - SAL;
+
+-- 5. 找出部门10中所有经理和部门20中的所有办事员的详细资料
+SELECT *
+FROM EMP
+WHERE (DEPTNO = 10 AND JOB = 'MANAGER') OR (DEPTNO = 20 AND JOB = 'CLERK');
+
+-- 6. 找出部门10中所有经理、部门20中所有办事员，既不是经理又不是办事员但其
+--    薪金 >=2000的所有雇员的详细资料
+SELECT *
+FROM EMP
+WHERE (DEPTNO = 10 AND JOB = 'MANAGER') OR
+      (DEPTNO = 20 AND JOB = 'CLERK') OR
+      (JOB != 'MANAGER' AND JOB != 'CLERK' AND SAL >= 2000);
+
+-- 7. 找出收取佣金的雇员的不同工作
+SELECT DISTINCT JOB
+FROM EMP
+WHERE COMM IS NOT NULL;
+
+-- 8. 找出不收取佣金或收取的佣金低于100的雇员
+SELECT *
+FROM EMP
+WHERE COMM IS NULL OR COMM < 100;
+
+-- 9. 找出早于12年之前受雇的雇员
+SELECT *
+FROM EMP
+WHERE (SYSDATE - HIREDATE) / 365 > 12;
+
+-- 10. 显示只有首字母大写的所有雇员的姓名
+SELECT SUBSTR(ENAME, 0, 1) || LOWER(SUBSTR(ENAME, 1, LENGTH(ENAME)-1)) 姓名
+SUBSTR(ENAME, 1, LENGTH(ENAME)-1)
+FROM EMP;
+
+-- 11. 显示正好为15个字符的雇员姓名
+SELECT ENAME 姓名
+FROM EMP
+WHERE LENGTH(ENAME) = 15;
+
+-- 12. 显示不带有'R'的雇员姓名
+SELECT ENAME 姓名
+FROM EMP
+WHERE ENAME NOT LIKE '%R%';
+
+-- 13. 显示所有雇员的姓名的前三个字符
+SELECT SUBSTR(ENAME, 0, 3) 姓名
+FROM EMP;
+
+-- 14. 显示所有雇员的姓名，用a替换所有'A'
+SELECT REPLACE(ENAME, 'A', 'a') 姓名
+FROM EMP;
+
+-- 15. 显示所有雇员的姓名以及满10年服务年限的日期
+SELECT ENAME 姓名, HIREDATE + 365 * 10 满10年服务年限的日期
+FROM EMP;
+
+-- 16. 显示雇员的详细资料，按姓名排序
+SELECT *
+FROM EMP
+ORDER BY ENAME;
+
+-- 17. 显示雇员姓名，根据其服务年限，将最老的雇员排在最前面
+SELECT ENAME 姓名, (SYSDATE - HIREDATE) / 365 服务年限
+FROM EMP
+ORDER BY 服务年限 DESC;
+
+-- 18. 显示所有雇员的姓名、工作和薪金，按工作内的工作的降序顺序排序，而工作
+--     按薪金排序
+SELECT ENAME 姓名, JOB 工作, SAL 薪金
+FROM EMP
+ORDER BY 工作 DESC, 薪金 ASC;
+
+-- 19. 显示所有雇员的姓名和加入公司的年份和月份，按雇员受雇日所在月排序，将
+--     最早年份的项目排在最前面
+
+-- 20. 显示在一个月为30天的情况下所有雇员的日薪金
+SELECT EMPNO 员工编号,
+       ENAME 姓名,
+       JOB 职位,
+       SAL 月薪,
+       SAL / 30 日薪,
+       DEPTNO 部门
+FROM EMP;
+
+-- 21. 找出在(任何年份的)2月受聘的所有雇员
+
+-- 22. 对于每个雇员，显示其加入公司的天数
+SELECT ENAME 姓名, SYSDATE - HIREDATE 加入公司天数
+FROM EMP;
+
+-- 23. 显示姓名字段的任何位置，包含"A"的所有雇员的姓名
+SELECT ENAME
+FROM EMP
+WHERE ENAME LIKE '%A%';
--- oracle/note/sql_base.sql
+++ oracle/note/sql_base.sql
@@ -26,3 +26,158 @@ CREATE TABLE USERS(
 
 -- 查看表的结构，但只能在命令行模式下使用
 DESC USERS;
+
+-- SQL语句不区分大小写，包括登录的用户名，密码也不区分大小写
+
+-- 在查询过程中，对于数值型的数据，可以执行 +, -, *, / 运算
+
+-- 可以给查询字段起别名，有不同的方式，双引号可选，as也可选，例
+SELECT EMPNO 员工编号,ENAME 员工姓名,SAL "月薪",SAL*12 AS "年薪"
+FROM EMP;
+
+-- 在算数表达式中出现NULL,得到的结果就是NULL,NULL不等于0.0
+
+-- 用 || 可以将两列或多列查询结果合并到一起, 例:
+SELECT EMPNO,ENAME,JOB,EMPNO||ENAME||JOB "员工信息"
+FROM EMP;
+
+-- 在连接表达式中出现字符数据
+-- 在连接表达式中出现NULL，就是原来的字符型数据
+SELECT EMPNO || ' 的经理是 ' || MGR AS "从属关系"
+FROM EMP;
+
+-- 任何的类型都支持NULL(空值)
+
+-- PL/SQL中对数据进行修改，可以在使用下面的select语句后，直接在查询结果集中
+-- 修改
+SELECT * FROM EMP
+FOR UPDATE;
+
+-- 对于日期型的数据可以使用 +, - 运算符
+-- 1. 一个日期 + - 一个数值(就是 + - 一个天数)，得到一个新的日期，例
+SELECT EMPNO,ENAME,HIREDATE "雇佣日期", (HIREDATE + 90) "转正日期"
+FROM EMP;
+
+-- 2. 两个日期型的数据相减，得到的是两者之间相差的天数,例
+SELECT EMPNO,ENAME, (SYSDATE - HIREDATE)/365 "工作的年限"
+FROM EMP;
+-- SYSDATE 取得当前的日期
+
+-- 3. 两个日期型的数据不能相加，日期型的数据不能进行乘除运算
+
+-------------------------------------------------------------
+-- 去除单列重复的数据
+SELECT DISTINCT DEPTNO
+FROM EMP;
+
+-- 去除多列重复的数据
+SELECT DISTINCT JOB, DEPTNO
+FROM EMP;
+
+--------------------------------------------------------------
+-- 使用WHERE子句对数据进行过滤
+SELECT *
+FROM EMP
+WHERE DEPTNO = 10;
+
+-- 对于字符型的数据和日期型的数据必须要使用 ''
+-- 字符型的数据对大小写是敏感的
+SELECT *
+FROM EMP
+WHERE ENAME = 'SMITH';
+
+-- 日期型的数据，格式是敏感的，使用日期型数据的默认格式是
+-- DD-MM-YYYY(日-月-年)
+-- 如果是中文版的Oracle，那么必须要在月份里加一个‘月’,英文版不需要
+SELECT *
+FROM EMP
+WHERE HIREDATE = '20-2月-1981';
+
+-- 改变当前会话中的日期格式
+alter session set nls_date_format="YYYY-MM-DD HH:MI:SS";
+
+-- 使用BETWEEN ... AND，会包含边界的
+SELECT *
+FROM EMP
+WHERE SAL BETWEEN 2000 AND 3000;
+-- 在结果中会包含SAL是2000和3000的结果,相当于
+SELECT *
+FROM EMP
+WHERE SAL >= 2000 AND SAL <= 3000;
+
+-- 使用IN
+SELECT *
+FROM EMP
+WHERE JOB IN('MANAGER', 'CLERK');
+-- 相当于
+SELECT *
+FROM EMP
+WHERE JOB = 'MANAGER' OR JOB = 'CLERK';
+
+-- LIKE 用于字符型数据的查询，可以执行模糊查询
+-- % 表示零个或多个任意的字符
+SELECT *
+FROM EMP
+WHERE ENAME LIKE '%A%';
+
+-- _ 表示一个任意字符
+SELECT *
+FROM EMP
+WHERE ENAME LIKE '_A%';
+
+-- 对于空值要使用 IS NULL 进行比较
+SELECT *
+FROM EMP
+WHERE COMM IS NULL;
+
+-- 使用NOT运算符
+SELECT *
+FROM EMP
+WHERE SAL NOT BETWEEN 2000 AND 3000;
+-- 相当于
+SELECT *
+FROM EMP
+WHERE SAL < 2000 OR SAL > 3000;
+
+-- SQL注入攻击, 由于NOT，AND，OR的运算优先级是 NOT>AND>OR，所以，即使用户
+-- 输入的密码不对，也可以查出想要的数据
+SELECT *
+FROM USERS
+WHERE NAME = 'admin' OR 'X'='X' AND PASSWORD='43423';
+
+-----------------------------------------------------------------------
+-- 对查询后的数据进行排序,使用ORDER BY 子句,默认使用指定的字段升序排序
+SELECT *
+FROM EMP
+WHERE SAL > 2000
+ORDER BY SAL;
+
+-- 显示的指定升降序
+-- 1. 升序
+SELECT *
+FROM EMP
+WHERE SAL > 2000
+ORDER BY SAL ASC;
+
+-- 2. 降序
+SELECT *
+FROM EMP
+WHERE SAL > 2000
+ORDER BY SAL DESC;
+
+-- 也可以使用字段的别名进行排序
+SELECT EMPNO, ENAME, (SAL * 12) "年薪"
+FROM EMP
+ORDER BY "年薪";
+
+-- 使用多个字段排序
+SELECT *
+FROM EMP
+WHERE SAL > 2000
+ORDER BY DEPTNO, SAL;
+------------------------------------------------------------------------
+
+-- 转义特出字符, ESCAPKE 是指定转移符
+SELECT *
+FROM EMP
+WHERE ENAME LIKE '%\%%' ESCAPE '\';
